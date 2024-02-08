b89da9cd4ae89c56646d5134f52b9c2cc7bb31fa
NEXUS\pventura <pventura@nexusgeographics.com>
2024-02-08 11:52:46 +0100

    first commit

---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi web personal </h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portafolio/">Portafolio</a></li>
+    <li><a href="/contacto/">Contacto</a></li>
+</ul>
+"""
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contacto(request):
+    return render(request, "core/contacto.html")
